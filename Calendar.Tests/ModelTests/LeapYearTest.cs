3c84c8b1a13569317dae3a5759a7989db10c3549
Crystal Fecteau and Tessa Sullivan <tessa@>
2019-04-22 11:07:28 -0700

    Initial program and testing files created, with incorrect versions referenced

---
+++ Calendar.Tests/ModelTests/LeapYearTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace Calendar.Tests
+{
+  [TestClass]
+  public class LeapYearTest
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      //testing code will go here
+    }
+  }
+}
---
+++ Calendar/Models/LeapYear.cs
@@ -0,0 +1,11 @@
+namespace Calendar
+{
+  public class LeapYear
+  {
+    public bool IsLeapYear(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
